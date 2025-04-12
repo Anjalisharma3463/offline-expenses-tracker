775b7a9ef4090fa39bf5d53213122c989eff9639
Anjali sharma <Anjali sharma>
2025-04-12 14:52:40 +0530

    added app.tsx code

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
+// src/App.tsx
+import AppLayout from "./components/layout/AppLayout"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Dashboard from "./pages/Dashboard"
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
+import { ToastProvider } from "./components/ui/toast"
+import { Toast } from "./components/ui/toast"
  
-import './App.css'
-
-function App() {
-  
+export default function App() {
   return (
-    <>
-  <p className=''>hii</p>     
-    </>
+    <ToastProvider> 
+    <AppLayout>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
+    </AppLayout>
+    <Toast />
+    </ToastProvider>
   )
 }
-
-export default App
